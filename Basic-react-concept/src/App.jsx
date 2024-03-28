c4142733d06b91a2daf2ce0c95a11b640156986f
Awais Ali <awaisali72500@gmail.com>
2024-03-28 06:18:50 +0500

    Added example of routing and how does nested routing works

--- Basic-react-concept/src/App.jsx
+++ Basic-react-concept/src/App.jsx
@@ -1,17 +1,20 @@
 import "./App.css";
-import "./input.css"
+import "./index.css"
 import Cards from "./components/Cards"
 import ProductTab from "./components/ProductTab";
 import ItemList from "./components/ListItem";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { Route ,Routes , Link} from "react-router-dom";
+
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
 
 import Home from "../src/components/Router/Home/Home"
 import Footer from "./components/Router/Footer/Footer"
 import Header from "./components/Router/Header/Header"
 import Contact from "./components/Router/Contact/Contact"
+// import Layout from "./components/Router/Layout";
 
 // function Greeting(){
 //   return (
@@ -26,35 +29,92 @@ import Contact from "./components/Router/Contact/Contact"
 function App() {
 
   // initilaze the Browser Router 
-  const Router= createBrowserRouter 
-  ([
+  // const Router= createBrowserRouter 
+  // ([
 
-    {
-      path: "/",
-      element: <Home />
-    },
-    {
-      path: "/",
-      element: <Header />
-    },
-    {
-      path: "/",
-      element: <Contact />
-    },
+    
+  //   {
+  //     path: "/",
+  //     element: <Home />
+  //   },
+  //   {
+  //     path: "/",
+  //     element: <Header />
+  //   },
+  //   {
+  //     path: "/",
+  //     element: <Contact />
+  //   },
 
-    {
-      path: "/",
-      element: <Footer />
-    },
+  //   {
+  //     path: "/",
+  //     element: <Footer />
+  //   },
 
-  ])
+  // ])
+
+  // using Layout approch 
+
+
+  // const router=createBrowserRouter([
+
+
+  //   {
+  //     // parent layout component 
+  //     element: < Layout />,
+  //     // children component route 
+
+  //     children:[
+  //       {
+  //         path: '/',
+  //         element: <Home />,
+  //       },
+
+  //       {
+  //         path: '/Contact',
+  //         element: <Contact />,
+  //       },
+
+
+
+  //     ],
+  //   },
+
+  // ])
+
   return (
     <div>
      
 
-  <h2 className="bg-"> React Routes</h2>
+  <h2> React Routes</h2>
 
-  <RouterProvider router={Router } />
+  <nav>
+
+    <ul>
+      <li> <Link to="/">Home</Link></li>
+      <li> <Link to="/Contact">Contact</Link></li>
+      <li> <Link to="/Header">Header</Link></li>
+      <li> <Link to="/Footer">Footer</Link></li>
+      
+
+    </ul>
+  </nav>
+
+  {/* <RouterProvider router={router } /> */}
+
+  <Routes>
+
+    <Route path="/" element={<Home />} />
+    <Route path="/Contact" element={<Contact />} />
+
+    <Route path="/Header" element={<Header />} />
+    <Route path="/Footer" element={<Footer />} />
+    {/* <Route path="/Footer/:id" element={<Footer />} /> */}
+
+
+
+
+  </Routes>
 
     </div>
     
--- Basic-react-concept/src/main.jsx
+++ Basic-react-concept/src/main.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './components/Router/Root.jsx'
+import ErrorPage from './components/Router/ErrorPage.jsx'
+import Contact from './components/Router/Contact/Contact.jsx'
 
+
+const router=createBrowserRouter([
+
+  {
+    path: "/",
+    element : <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+
+        path: "/Contact/:Contactid",
+        element : <Contact />
+      },
+
+    ],
+
+  },
+ 
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* <BrowserRouter>
+    
     <App />
+    
+    </BrowserRouter> */}
+
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
